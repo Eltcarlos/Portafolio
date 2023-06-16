194fab5386d4500a25c5b3fe4ab5986a67382a84
Carlos Diaz <104026079+Eltcarlos@users.noreply.github.com>
2023-06-16 00:04:08 -0500

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -37,7 +37,7 @@ const about = () => {
   return (
     <>
       <Head>
-        <title>CodeBucks | About Page</title>
+        <title>About Page</title>
         <meta name="description" content="any description" />
       </Head>
       <TransitionEffect />
